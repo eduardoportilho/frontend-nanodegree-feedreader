08597564da71ad1588641b747f690c67539cf8db
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-04-13 20:39:14 +0200

    Test: Ensures that each feed has a URL defined and that the URL is not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,12 +26,15 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
+        /* Ensures that each feed has a URL defined
          * and that the URL is not empty.
          */
-
+        it('has non-empty URLs', function() {
+            for(var i = 0; i < allFeeds.length ; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
